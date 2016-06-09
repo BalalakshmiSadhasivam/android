eb8d879a084faa5cde140274b684032c4b3deb7a
Balalakshmi Sadhasivam <balalakshmisadhasivam@gmail.com>
2016-06-09 12:32:46 +0530

    Customised viewpager file added
    Viewpager in which swiping is disabled.

---
+++ NonSwipeableViewPager.java
@@ -0,0 +1,37 @@
+package com.zoho.myapp.util;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by bala-3019 on 09/06/16.
+ * <p/>
+ * <p/>
+ * <p/>
+ * Viewpager in which swiping  is disabled. Tabs should be touched instead of swiping the viewpager.
+ */
+
+public class NonSwipeableViewPager extends ViewPager {
+
+    public NonSwipeableViewPager(Context context) {
+        super(context);
+    }
+
+    public NonSwipeableViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        // Never allow swiping to switch between pages
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // Never allow swiping to switch between pages
+        return false;
+    }
+}
